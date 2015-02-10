fc72fc50a75a37b334b56266e9ff8a7d33fc893d
Zack Piper <zack.piper123@gmail.com>
2015-02-10 16:56:38 +0000

    This looks better.

--- apis/v1/time.rb
+++ apis/v1/time.rb
@@ -1,9 +1,7 @@
-module DNT
-  class APIv1 < Grape::API
-    desc "Return the time"
-    get :time do
-      time = Time.now.getutc
-      { year: time.year,
+class Time
+  def getall
+    time = self
+    { year: time.year,
       month: time.month,
       day: time.day,
       wday: time.wday,
@@ -13,23 +11,21 @@ module DNT
       sec: time.sec,
       usec: time.usec,
       zone: time.zone }
+  end
+end
+
+module DNT
+  class APIv1 < Grape::API
+    desc "Return the time"
+    get :time do
+      Time.now.getutc.getall
     end
 
     params do
       requires :zone, type: String, desc: 'The timezone'
     end
     get :timezone do
-      time = TZInfo::Timezone.get(params[:zone]).now
-      { year: time.year,
-      month: time.month,
-      day: time.day,
-      wday: time.wday,
-      yday: time.yday,
-      hour: time.hour,
-      min: time.min,
-      sec: time.sec,
-      usec: time.usec,
-      zone: time.zone }
+      TZInfo::Timezone.get(params[:zone]).now.getall
     end
   end
 end
